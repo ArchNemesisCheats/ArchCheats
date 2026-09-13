5e5b1fa7d8496a325393205a721de9463b011275
ArchNemesisCheats <enhancementsarchhub@gmail.com>
2026-09-12 20:03:20 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,18 +1,1 @@
-if (getgenv().UC_LOADED) then
-	return;
-end;
-getgenv().UC_LOADED = true;
-
-if (identifyexecutor() == "Wave") then
-	getgenv().gethui = function()
-		return game:GetService("CoreGui");
-	end;	
-end;
-
-if (game.GameId == 6035872082) then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2f956196b25b2caa970255b3d08949b6.lua"))()
-elseif (game.GameId == 1008451066) then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2f956196b25b2caa970255b3d08949b6.lua"))()
-elseif (game.PlaceId == 2317712696) then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2f956196b25b2caa970255b3d08949b6.lua"))()
-end;
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2f956196b25b2caa970255b3d08949b6.lua"))()
